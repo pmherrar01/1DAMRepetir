6d6957f8e4e2770297f2dcb3592b673b7d2c5bd0
pmherrar01 <120454320+pmherrar01@users.noreply.github.com>
2025-01-09 21:34:16 +0100

    pruebas

--- Programacion/SegundoTrimestre/prueba/src/Main.java
+++ Programacion/SegundoTrimestre/prueba/src/Main.java
@@ -1,5 +1,19 @@
 
 public class Main {
+
+    public static void funcion1(int num){
+
+        System.out.println(num);
+
+    }
+    public static int suma(int a, int b){
+        int suma;
+
+        suma = a + b;
+
+        return suma;
+    }
+
     public static void main(String[] args) {
         System.out.printf("Hola mundo");
         System.out.println("Probando");
@@ -7,9 +21,30 @@ public class Main {
         System.out.println("Haciendo mas pruebas1");
         System.out.println("Haciendo mas pruebas2");
 
-        int num = 5;
+        int num = 5, a = 0;
         char c  = 'c';
         boolean bandera = true;
+
+        a++; //== a = a + 1
+        a += 5; // a = a + 5
+
+        // O == ||
+        if (bandera || !bandera){
+            System.out.println("Hola");
+        }
+        // y == &&
+        if(bandera && !bandera){
+            System.out.println("Hola");
+        }
+        // NO == !
+        if(!bandera){
+            System.out.println("Hola");
+        }
+        //distinto == !=
+        if(bandera != !bandera){
+            System.out.println("Hola");
+        }
+
 
         if(!bandera) {
             System.out.println("Hola");
@@ -17,8 +52,8 @@ public class Main {
             System.out.println("Adios");
         }
 
-        while (bandera){
-            System.out.println("infinito");
-        }
+        funcion1(num);
+
+        suma(5,5);
     }
 }
