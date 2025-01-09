ead7059c1790e34a35a0aec3c08ffac1a5ecf619
pmherrar01 <120454320+pmherrar01@users.noreply.github.com>
2025-01-09 20:51:24 +0100

    if

--- Programacion/SegundoTrimestre/prueba/src/Main.java
+++ Programacion/SegundoTrimestre/prueba/src/Main.java
@@ -6,5 +6,15 @@ public class Main {
         System.out.println("Haciendo mas pruebas");
         System.out.println("Haciendo mas pruebas1");
         System.out.println("Haciendo mas pruebas2");
+
+        int num = 5;
+        char c  = 'c';
+        boolean bandera = true;
+
+        if(!bandera) {
+            System.out.println("Hola");
+        }else {
+            System.out.println("Adios");
+        }
     }
 }
