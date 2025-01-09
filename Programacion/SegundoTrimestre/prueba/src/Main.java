268b8aff137bd87ca79162a28a033d7d8d539649
pmherrar01 <120454320+pmherrar01@users.noreply.github.com>
2025-01-09 20:46:08 +0100

    cambios

--- Programacion/SegundoTrimestre/prueba/src/Main.java
+++ Programacion/SegundoTrimestre/prueba/src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hola mundo");
-
+        System.out.println("Probando");
+        System.out.println("Haciendo mas pruebas");
     }
 }
