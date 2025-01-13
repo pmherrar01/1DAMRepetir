15a79ad84723a5a2ae9904cffff9baebb57f7d5f
pmherrar01 <120454320+pmherrar01@users.noreply.github.com>
2025-01-13 21:31:12 +0100

    ejercicios por terminar

---
+++ Programacion/SegundoTrimestre/ejercicios_bloque_1/ejercicio1/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int x= 5, y=8;
+        double n = 5.2, m = 9.6;
+
+
+        System.out.println(cVariables);
+
+    }
+}
