805403886daaade6a28d931e3a06360c75af24ca
pmherrar01 <120454320+pmherrar01@users.noreply.github.com>
2025-01-20 21:16:50 +0100

    f

--- Programacion/SegundoTrimestre/prueba/src/Main.java
+++ Programacion/SegundoTrimestre/prueba/src/Main.java
@@ -66,5 +66,33 @@ public class Main {
         int num6;
         Scanner entrada  = new Scanner(System.in);
         a = entrada.nextInt();
+
+        String nombre = "pablo";
+        System.out.println(nombre.length());  //muestra la longitud de una variable
+
+        System.out.println("hola mundo".length()); //muestra la longitud de una cadena de texto
+
+        System.out.println("hola mundo".charAt(5)); //muestra el caracter de la posicion 5 de la cadena de texto
+
+        System.out.println("hola mundo".substring(4)); //muestra una subcadena a partir del indice que indiquemos en este caso el 4
+
+        System.out.println("hola mundo".substring(3,8)); // muestra una subcadena que inicia en el indice que le indiquemos hasta el indice que le indiquemos en este caso desde el indice 3 hasta el inidice 8
+
+        String s =  "Java desde Cero";
+        int salida = s.indexOf("Cero"); // retorna 11 que es la posicion en la que se encuentra la cadena "Cero"
+        System.out.println(salida);
+
+        String sa =  "Java desde Cero";
+        int salida2 = s.indexOf('a', 3);
+        System.out.println(salida2);
+
+        System.out.println("hola".equals("Hola"));// compara cadenas
+
+        String op = "Ana";
+        String po = "O";
+
+        int compare = op.compareTo(po);
+        System.out.println(compare);
     }
+
 }
