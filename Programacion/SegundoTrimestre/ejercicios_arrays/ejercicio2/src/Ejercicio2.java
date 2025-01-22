7ea4b5e18b5d62144ac70ada7ec227076d42273a
pmherrar01 <120454320+pmherrar01@users.noreply.github.com>
2025-01-22 17:58:22 +0100

    Ejercicio2 por terminar

---
+++ Programacion/SegundoTrimestre/ejercicios_arrays/ejercicio2/src/Ejercicio2.java
@@ -0,0 +1,19 @@
+//Escriba un programa que almacene en un array 15 números aleatorios double
+//comprendidos entre 1.00 y 50.00. Mostrar el contenido del array separando los
+//números por guión.
+//Para redondear a dos decimales podemos utilizar:
+//Math.rint(number * 100) / 100
+//Para redondear a un decimal: multiplicamos y dividimos por 10
+//Para redondear a tres decimales: multiplicamos y dividimos por 1000
+//Así sucesivamente
+public class Ejercicio2 {
+    public static final int num = 15;
+
+    public static double rellenarVector(double[] vNumeros){
+
+    }
+
+    public static void main(String[] args) {
+        double[] vNumeros = new double[num];
+    }
+}
