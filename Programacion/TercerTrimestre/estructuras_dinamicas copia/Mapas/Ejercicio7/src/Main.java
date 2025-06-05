7939ac8b949b06c802c5357f52991cba96abce59
pmherrar01 <pmherrerar01@educarex.es>
2025-06-05 18:53:46 +0200

    7 por terminar

--- Programacion/TercerTrimestre/estructuras_dinamicas copia/Mapas/Ejercicio7/src/Main.java
+++ Programacion/TercerTrimestre/estructuras_dinamicas copia/Mapas/Ejercicio7/src/Main.java
@@ -1,5 +1,14 @@
+//Un supermercado nos ha pedido hacer un programa para vender su mercancía. En esta primera versión del programa se tendrán en cuenta los productos que se indican en la tabla junto con su precio. Cuando se realiza la compra,
+// hay que indicar el producto y el número de unidades que se compran, por ejemplo “guisantes” si se quiere comprar un bote de guisantes y la cantidad, por ejemplo “3” si se quieren comprar 3 botes.
+// La compra se termina con la palabra “fin”.
+// Utiliza una estructura de datos para almacenar los nombres y precios de los productos y otra estructura para almacenar la compra que realiza el usuario.
+// A continuación se muestra una tabla con los productos disponibles y sus respectivos precios:
+//
+//Ten en cuenta que si algún producto se repite en diferentes líneas, se deben agrupar en una sola.
+// Por ejemplo, si se pide primero 1 bote de tomate y luego 3 botes de tomate, en el extracto se debe mostrar que se han pedido 4 botes de tomate.
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
     }
 }
